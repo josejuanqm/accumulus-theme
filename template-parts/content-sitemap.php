355c6419b15c827fec5f8b08c8b38f8e0081a6c4
Jose Quintero <jose.juan.qm@gmail.com>
2024-05-29 11:08:41 -0600

    added docker, sitemap and terms changes

---
+++ template-parts/content-sitemap.php
@@ -0,0 +1,54 @@
+<section class="section">
+  <div class="container mx-auto px-4 lg:px-0">
+    <div class="grid grid-cols-12 gap-x-s6 py-s6 md:py-s8">
+      <h1 class="text-h1Mobile md:text-h1Tablet lg:text-h1 col-span-12 md:col-span-5 lg:col-span-5 col-start-1 md:col-start-1 lg:col-start-2 pb-s8 lg:pb-0">
+        <?php the_title(); ?>
+      </h1>
+
+      <div class="flex flex-row justify-between md:pb-s8 col-span-6 md:col-span-3 lg:col-span-2 col-start-1 md:col-start-6 lg:col-start-8">
+        <ul class="flex flex-col items-start gap-s2 list-none">
+          <li><p class="text-neutral-dgray text-h6Mobile md:text-h6Tablet lg:text-h6">Resources</p></li>
+          <li><a class="text-neutral-dgray text-b3Mobile md:text-b3Tablet lg:text-b3" href="">About Us</a></li>
+          <li><a class="text-neutral-dgray text-b3Mobile md:text-b3Tablet lg:text-b3" href="">Our Team</a></li>
+          <li><a class="text-neutral-dgray text-b3Mobile md:text-b3Tablet lg:text-b3" href="">News</a></li>
+          <li><a class="text-neutral-dgray text-b3Mobile md:text-b3Tablet lg:text-b3" href="">Events</a></li>
+          <li><a class="text-neutral-dgray text-b3Mobile md:text-b3Tablet lg:text-b3" href="">Case For Change</a></li>
+          <li><a class="text-neutral-dgray text-b3Mobile md:text-b3Tablet lg:text-b3" href="">Regulator Forum</a></li>
+          <li><a class="text-neutral-dgray text-b3Mobile md:text-b3Tablet lg:text-b3" href="">Careers</a></li>
+        </ul>
+      </div>
+
+      <div class="flex flex-row justify-between md:pb-s8 col-span-6 md:col-span-3 lg:col-span-2 col-start-7 md:col-start-9 lg:col-start-10">
+        <ul class="flex flex-col items-start gap-s2 list-none">
+          <li><p class="text-neutral-dgray text-h6Mobile md:text-h6Tablet lg:text-h6">Platform</p></li>
+          <li><a class="text-neutral-dgray text-b3Mobile md:text-b3Tablet lg:text-b3" href="">Accumulus Platform</a></li>
+          <li><a class="text-neutral-dgray text-b3Mobile md:text-b3Tablet lg:text-b3" href="">Core Capabilities</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="grid grid-cols-12 gap-x-s6 pb-s6 md:py-s8">
+      <ul class="flex flex-col items-start gap-s2 list-none row-start-4 md:row-auto col-span-6 md:col-span-5 lg:col-span-5 col-start-1 md:col-start-1 lg:col-start-2 py-s6 md:py-0 md:pb-s8">
+        <li><a href="" class="text-neutral-dgray text-b3Mobile md:text-b3Tablet lg:text-b3">LinkedIn</a></li>
+        <li><a href="" class="text-neutral-dgray text-b3Mobile md:text-b3Tablet lg:text-b3">Contact Us</a></li>
+      </ul>
+
+      <div class="flex flex-row justify-between md:pb-s8 col-span-6 md:col-span-3 lg:col-span-2 col-start-1 md:col-start-6 lg:col-start-8">
+        <ul class="flex flex-col items-start gap-s2 list-none">
+          <li><p class="text-neutral-dgray text-h6Mobile md:text-h6Tablet lg:text-h6">Resources</p></li>
+          <li><a class="text-neutral-dgray text-b3Mobile md:text-b3Tablet lg:text-b3" href="">E-books</a></li>
+          <li><a class="text-neutral-dgray text-b3Mobile md:text-b3Tablet lg:text-b3" href="">Thought Leadership</a></li>
+          <li><a class="text-neutral-dgray text-b3Mobile md:text-b3Tablet lg:text-b3" href="">Media</a></li>
+        </ul>
+      </div>
+
+      <div class="flex flex-row justify-between md:pb-s8 col-span-6 md:col-span-3 lg:col-span-2 col-start-7 md:col-start-9 lg:col-start-10">
+        <ul class="flex flex-col items-start gap-s2 list-none">
+          <li><p class="text-neutral-dgray text-h6Mobile md:text-h6Tablet lg:text-h6">Legal</p></li>
+          <li><a class="text-neutral-dgray text-b3Mobile md:text-b3Tablet lg:text-b3" href="">Privacy Policy</a></li>
+          <li><a class="text-neutral-dgray text-b3Mobile md:text-b3Tablet lg:text-b3" href="">Terms & Conditions</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</section>
---
+++ template-parts/content-terms-and-conditions.php
@@ -0,0 +1,19 @@
+<section class="section bg-cover bg-bottom" style="background-image: url(<?php bloginfo('template_url'); ?>/images/legal/hero.jpg);">
+  <div class="container mx-auto px-4 lg:px-0">
+    <div class="grid grid-cols-12 gap-x-s6 py-s6 md:py-s8">
+      <h1 class="text-h1Mobile md:text-h1Tablet lg:text-h1 col-span-12">
+        <?php the_title(); ?>
+      </h1>
+    </div>
+  </div>
+</section>
+
+<section class="section">
+  <div class="container mx-auto px-4 lg:px-0">
+    <div class="grid grid-cols-12 gap-x-s6 py-s6 md:py-s8">
+      <div class="col-span-12 flex flex-col gap-s4 [&>h3]:text-h3Mobile md:[&>h3]:text-h3Tablet lg:[&>h3]:text-h3 [&>h4]:text-h4Mobile md:[&>h4]:text-h4Tablet lg:[&>h4]:text-h4 [&>h5]:text-h5Mobile md:[&>h5]:text-h5Tablet lg:[&>h5]:text-h5">
+        <?php the_content(); ?>
+      </div>
+    </div>
+  </div>
+</section>
