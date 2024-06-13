316cb970a69303aec9c3cb5ed0aff5517de5dec7
Luis Barrera <autonomodeguate@gmail.com>
2024-06-13 11:01:26 -0600

    feat: get started page - updates

--- template-parts/content-get-started.php
+++ template-parts/content-get-started.php
@@ -31,7 +31,7 @@
 				<p class="text-b2 md:max-w-550 lg:max-w-full"><?php echo $resume_text; ?></p>
 			</div>
 			<div class="col-span-12 lg:col-span-6 lg:col-start-6 flex flex-col lg:flex-row gap-s2 lg:gap-s4">
-				<a href="<?php echo $link_learn_more; ?>" class="btn-tertiary-white">Our mission</a>
+				<a href="<?php echo $link_learn_more; ?>" class="btn-secondary-inverted">Our mission</a>
 			</div>
 
 		</div>
@@ -178,5 +178,25 @@ $why_values = get_field('why_values');
 
 <!-- Values -->
 
-<?php include get_theme_file_path( 'components/get-started-section.php' ); ?>
+<section class="relative section w-full pt-s12 pb-s12 bg-neutral-nude bg-get-started-section-mobile md:bg-get-started-section-tablet lg:bg-get-started-section-desktop bg-cover bg-no-repeat bg-center">
+	<div class="container mx-auto">
+
+		<div class="grid grid-cols-12 gap-x-s2 gap-y-s3 md:gap-y-s6">
+
+			<h6 class="col-span-12 text-h4Mobile md:text-h4Tablet lg:text-h4 uppercase">GET STARTED</h6>
+			<h2 class="col-span-12 text-h2Mobile md:text-h2Tablet lg:text-h2">Ready to Join Us?</h2>
+			<p class="col-span-12 md:col-span-4 text-b3Mobile md:text-b3Tablet lg:text-b3 py-s2 md:py-0">
+				Take the first step toward transforming the life sciences-regulatory ecosystem.
+				<br /><br />
+				Together, let’s make a difference!
+			</p>
+			<div class="col-span-12 flex justify-start">
+				<a href="#" class="btn-tertiary">Get Started</a>
+			</div>
+
+		</div>
+
+	</div>
+</section>
+<?php //include get_theme_file_path( 'components/get-started-section.php' ); ?>
 <!-- Get statted section -->
