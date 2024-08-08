2ba47d5fe649bec1b13cb05df85e8fcb651e91d1
Luis Barrera <autonomodeguate@gmail.com>
2024-08-08 12:23:58 -0600

    fix: tickets from 404 page

--- 404.php
+++ 404.php
@@ -12,11 +12,11 @@ get_header();
 
 	<main id="primary" class="site-main">
 
-	<section class="section w-full lg:pt-72 pb-s4 md:pb-s7 lg:pb-s12 bg-primary-violet min-h-screen bg-no-repeat bg-left-top bg-cover" style="background-image: url(<?php bloginfo( 'template_url' ); ?>/images/bg-404.png);">
+	<section class="section w-full lg:pt-28 pb-s4 md:pb-s7 lg:pb-s12 bg-primary-violet min-h-screen bg-no-repeat bg-left-top bg-cover max-lg:h-screen" style="background-image: url(<?php bloginfo( 'template_url' ); ?>/images/bg-404.png);">
 
-		<div class="container mx-auto">
+		<div class="container mx-auto max-lg:h-[90%]">
 
-			<div class="grid grid-cols-12 gap-x-4 gap-y-s6 pt-40 md:pt-52 lg:pt-0 max-lg:content-between max-lg:h-screen">
+			<div class="grid grid-cols-12 gap-x-4 gap-y-s6 pt-24 md:pt-s6 lg:pt-0 max-lg:content-between max-lg:min-h-full">
 
 				<div class="col-span-12 flex flex-col gap-s1">
 					<h3 class="text-h8Mobile md:text-h8Tablet lg:text-h8 text-neutral-nwhite">404</h3>
@@ -28,7 +28,7 @@ get_header();
 				</div>
 
 				<div class="col-span-12 lg:col-span-4 lg:col-start-8 flex flex-col gap-s3 md:gap-s8 lg:gap-s4">
-					<p class="text-b2 text-neutral-nwhite max-lg:max-w-[380px]">This page has moved, is under construction, or doesn’t exist.</p>
+					<p class="body-2 text-neutral-nwhite max-lg:max-w-[380px]">This page has moved, is under construction, or doesn’t exist.</p>
 					<a class="btn-secondary-inverted" href="<?php echo get_home_url(); ?>">Back to Home</a>
 				</div>
 
