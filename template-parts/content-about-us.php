0c897c7772a0ba2ed400cd63a70cbac62f9d1123
Luis Barrera <autonomodeguate@gmail.com>
2024-08-20 22:25:37 -0600

    fix: update bugs - about page

--- template-parts/content-about-us.php
+++ template-parts/content-about-us.php
@@ -16,7 +16,7 @@
 	$link_learn_more = get_field('link_learn_more');
 ?>
 
-<section class="relative section w-full pt-s3 md:pt-s14 lg:pt-52 2xl:pt-60 pb-s12 md:pb-s7 lg:pb-s12 bg-secondary-carbon">
+<section class="relative section w-full pt-s3 md:pt-s14 lg:pt-52 2xl:pt-60 pb-s12 md:pb-s7 lg:pb-s12 bg-neutral-nwhite">
 	<picture class="absolute top-0 left-0 w-full h-full">
 		<source media="(min-width:1024px)" srcset="<?php echo $bg_image_for_desktop; ?>">
 		<source media="(min-width:768px)" srcset="<?php echo $bg_image_for_tablet; ?>">
@@ -28,6 +28,7 @@
 			<h1 class="col-span-12 heading-1"><?php echo $main_title; ?></h1>
 			<div class="col-span-12 md:col-span-6 md:col-start-6 flex flex-col md:items-end lg:items-start gap-s8 lg:gap-s4">
 				<p class="body-2 md:max-w-550 lg:max-w-full"><?php echo $resume_text; ?></p>
+        <a href="<?php echo $link_learn_more ?>" class="btn-secondary">Learn Why</a>
 			</div>
 		</div>
 	</div>
@@ -63,7 +64,7 @@
     <div class="container mx-auto px-s2">
       <div class="grid grid-cols-12 gap-x-s2 gap-y-s6">
         <div class="relative flex flex-col col-span-12 md:col-span-6 lg:col-span-6 items-start gap-s3">
-          <h4 class="text-b3 lg:heading-4 uppercase pt-s1">Our Story</h4>
+          <h4 class="heading-4 uppercase pt-s1">Our Story</h4>
           <div class="flex flex-row flex-nowrap overflow-visible w-full">
             <div id="slider-content-container" class="flex flex-row items-start justify-start w-full basis-full shrink-0">
               <?php foreach ($years as $index=>$year) { ?>
@@ -381,6 +382,8 @@
   }
 </script>
 
+<!-- End timeline -->
+
 <div class="py-s9 relative isolate overflow-hidden">  
   <svg class="absolute bottom-0 right-0" width="574" height="788" viewBox="0 0 574 788" fill="none" xmlns="http://www.w3.org/2000/svg">
   <path d="M946.726 370.449C941.515 370.449 931.694 370.853 926.483 371.659C804.622 386.172 667.93 302.72 648.288 142.466C638.668 62.6419 570.923 0.757812 488.948 0.757812C400.358 0.757812 328.405 72.9223 328.405 162.221C328.405 179.758 331.211 196.489 336.422 212.413C377.91 340.011 361.876 442.614 168.663 506.111C71.2551 538.363 0.503906 630.483 0.503906 739.738C0.503906 875.399 109.938 985.46 244.826 985.46C379.714 985.46 489.148 875.399 489.148 739.738C489.148 701.237 480.129 665.155 464.496 632.701C452.871 608.512 444.252 586.137 438.239 565.576C435.834 559.528 434.03 553.078 432.427 546.829C432.227 545.62 431.826 544.41 431.625 543.201C430.824 540.177 430.222 537.153 429.621 533.928C428.819 529.493 428.218 525.26 427.617 521.632C427.617 520.624 427.416 519.616 427.416 518.608C426.013 505.909 426.615 497.644 426.615 497.644C426.414 461.965 437.638 428.705 457.08 400.888C489.349 352.912 543.865 321.265 605.798 321.265C607.802 321.265 610.007 321.466 612.011 321.466C621.631 321.466 631.653 322.676 641.474 324.692C643.077 324.893 644.48 325.498 646.284 325.901C649.892 326.707 653.299 327.514 656.907 328.723C661.316 330.134 665.726 331.747 669.935 333.359C670.736 333.561 671.939 333.964 672.741 334.367C675.947 335.778 679.154 337.189 682.361 338.6C700.801 347.067 718.238 358.153 733.871 371.256C735.475 372.667 736.878 373.876 738.682 375.086C740.285 376.497 742.089 377.908 743.492 379.319C784.179 414.192 811.237 463.175 821.458 517.399C832.282 574.647 882.188 618.187 942.517 618.187C1010.46 618.187 1065.78 562.754 1065.78 494.218C1065.78 425.681 1015.67 372.062 947.327 370.651" fill="#F5F5F5"/>
@@ -392,7 +395,7 @@
       'bg_color' => 'bg-transparent',
       'titled' => true,
       'main_quote' => 'Expert Excerpts',
-      'sub_quote' => '“Filing a New Drug Application may have shifted from driving a truckload of paper to the relevant regulatory authority to FedEx-ing a CD-ROM to uploading a set of PDFs through the Electronic Submissions Gateway, but the underlying processes remain little changed. It’s long past time to play catchup.”',
+      'sub_quote' => '“Whether you’re developing drugs or approving them, the regulatory industry has so much to offer. Connecting the ecosystem will only amplify our impact for both patients and users.”',
       'author' => 'Francisco Nogueira',
       'position' => 'Chief Executive Officer, at Accumulus Synergy',
       'image' => 'https://via.placeholder.com/224',
@@ -400,6 +403,9 @@
     )
   ); ?>
 </div>
+
+<!-- End expert excerpts -->
+
 
 <section class="section relative w-full py-s10 bg-secondary-aqua flex flex-col gap-s9">
   <div class="container mx-auto">
@@ -415,81 +421,66 @@
       </h2>
     </div>
     <div class="grid grid-cols-12 gap-x-s2 gap-y-s6 md:gap-y-s8 lg:py-s6 pt-s4 md:pt-0">
-      <div class="col-span-12 md:col-span-5">
+      <div class="col-span-12 md:col-span-10 md:col-start-2 lg:col-span-5 lg:col-start-1">
         <div class="grid grid-cols-[64px, 1fr] gap-y-s2 lg:gap-y-s6 gap-x-s2 items-start">
           <div class="flex flex-col items-center justify-center col-start-1 col-span-1">                
-<svg width="65" height="65" viewBox="0 0 65 65" fill="none" xmlns="http://www.w3.org/2000/svg">
-<rect x="0.734375" y="0.734375" width="64" height="64" rx="10.9714" fill="#F5F5F5"/>
-<path d="M33.2344 48.7311C41.795 48.7311 48.7344 41.7917 48.7344 33.2311C48.7344 32.4793 48.6788 31.7406 48.5742 31.0182H43.9327C44.0798 31.7341 44.1582 32.4728 44.1582 33.2311C44.1582 39.2553 39.2585 44.155 33.2344 44.155C27.2102 44.155 22.3105 39.2553 22.3105 33.2311C22.3105 27.9653 26.0531 23.5591 31.0182 22.5328V35.444H39.873V31.015H35.444V17.7344H33.2311C32.4793 17.7344 31.7406 17.7899 31.0182 17.8945C23.5068 18.9699 17.7344 25.4255 17.7344 33.2344C17.7344 41.795 24.6737 48.7344 33.2344 48.7344V48.7311Z" fill="#444444"/>
-</svg>
+            <svg width="65" height="65" viewBox="0 0 65 65" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <rect x="0.390625" y="0.625" width="64" height="64" rx="10.9714" fill="#F5F5F5"/>
+              <path d="M43.6444 21.7384C39.4931 17.5872 32.7359 17.5872 28.5847 21.7384L25.9914 24.3317L23.4645 26.8586L20.8712 29.4519C16.72 33.6031 16.72 40.3603 20.8712 44.5116C25.0225 48.6628 31.7797 48.6628 35.9309 44.5116L38.5242 41.9183L41.0511 39.3914L43.6444 36.7981C47.7956 32.6469 47.7956 25.8897 43.6444 21.7384ZM38.4859 36.8262L35.959 39.3531L33.3657 41.9464C30.6272 44.685 26.1725 44.685 23.4364 41.9464C20.7004 39.2078 20.6978 34.7531 23.4364 32.0171L26.0297 29.4238L28.4699 31.8641L30.9969 34.391L33.5238 36.918L36.0508 34.391L33.5238 31.8641L30.9969 29.3371L28.5566 26.8969L31.1499 24.3036C33.8885 21.565 38.3431 21.565 41.0792 24.3036C43.8178 27.0422 43.8178 31.4969 41.0792 34.2329L38.4859 36.8262Z" fill="#444444"/>
+            </svg>
           </div>
           <h3 class="heading-2 col-start-2 col-span-1">
-          13 Years
+            140 NRAs
           </h3>
-          <p class="body-2 col-start-2 col-span-1 row-start-2">
-          It takes 13 years (on average) to bring a new drug to market after its initial discovery.¹
-          </p>
+          <div class="col-start-2 col-span-1 row-start-2">
+            <p class="body-2 lg:max-w-[368px]">
+              There are more than 140 national regulatory authority (NRA) jurisdictions that review and approve each drug application.
+            </p>
+          </div>
         </div>
       </div>
-      <div class="col-span-12 md:col-span-5 md:col-start-7">
+      <div class="col-span-12 md:col-span-10 lg:col-span-7 md:col-start-2 lg:col-start-7">
         <div class="grid grid-cols-[64px, 1fr] gap-y-s2 lg:gap-y-s6 gap-x-s2 items-start">
           <div class="flex flex-col items-center justify-center col-start-1 col-span-1">                
-<svg width="65" height="65" viewBox="0 0 65 65" fill="none" xmlns="http://www.w3.org/2000/svg">
-<rect x="0.734375" y="0.734375" width="64" height="64" rx="10.9714" fill="#F5F5F5"/>
-<path d="M23.6393 25.9883H18.2031V29.6133H23.6393V25.9883Z" fill="#444444"/>
-<path d="M18.2031 38.6705C18.2031 39.6711 19.0137 40.4817 20.0143 40.4817C21.0148 40.4817 21.8255 39.6711 21.8255 38.6705C21.8255 37.67 21.0148 36.8594 20.0143 36.8594C19.0137 36.8594 18.2031 37.67 18.2031 38.6705Z" fill="#444444"/>
-<path d="M34.5143 40.4817C35.5146 40.4817 36.3255 39.6708 36.3255 38.6705C36.3255 37.6703 35.5146 36.8594 34.5143 36.8594C33.514 36.8594 32.7031 37.6703 32.7031 38.6705C32.7031 39.6708 33.514 40.4817 34.5143 40.4817Z" fill="#444444"/>
-<path d="M49.0143 40.4817C50.0146 40.4817 50.8255 39.6708 50.8255 38.6705C50.8255 37.6703 50.0146 36.8594 49.0143 36.8594C48.014 36.8594 47.2031 37.6703 47.2031 38.6705C47.2031 39.6708 48.014 40.4817 49.0143 40.4817Z" fill="#444444"/>
-<path d="M38.1393 25.9883H32.7031V29.6133H38.1393V25.9883Z" fill="#444444"/>
-<path d="M52.6315 25.9883H47.1953V29.6133H52.6315V25.9883Z" fill="#444444"/>
-<path d="M16.3906 47.7318H23.638V44.1068H16.3906V22.3594H23.638V18.7344H16.3906C14.3895 18.7344 12.7656 20.3583 12.7656 22.3594V44.1068C12.7656 46.1079 14.3895 47.7318 16.3906 47.7318Z" fill="#444444"/>
-<path d="M38.138 18.7383H30.8906C28.8895 18.7383 27.2656 20.3622 27.2656 22.3633V44.1107C27.2656 46.1118 28.8895 47.7357 30.8906 47.7357H38.138V44.1107H30.8906V22.3633H38.138V18.7383Z" fill="#444444"/>
-<path d="M45.3828 18.7383C43.3817 18.7383 41.7578 20.3622 41.7578 22.3633V44.1107C41.7578 46.1118 43.3817 47.7357 45.3828 47.7357H52.6328V44.1107H45.3828V22.3633H52.6328V18.7383H45.3828Z" fill="#444444"/>
-</svg>
+            <svg width="65" height="65" viewBox="0 0 65 65" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <rect x="0.890625" y="0.625" width="64" height="64" rx="10.9714" fill="#F5F5F5"/>
+              <path d="M23.7955 25.8789H18.3594V29.5039H23.7955V25.8789Z" fill="#444444"/>
+              <path d="M18.3594 38.5612C18.3594 39.5617 19.17 40.3723 20.1705 40.3723C21.1711 40.3723 21.9817 39.5617 21.9817 38.5612C21.9817 37.5606 21.1711 36.75 20.1705 36.75C19.17 36.75 18.3594 37.5606 18.3594 38.5612Z" fill="#444444"/>
+              <path d="M34.6705 40.3723C35.6708 40.3723 36.4817 39.5614 36.4817 38.5612C36.4817 37.5609 35.6708 36.75 34.6705 36.75C33.6703 36.75 32.8594 37.5609 32.8594 38.5612C32.8594 39.5614 33.6703 40.3723 34.6705 40.3723Z" fill="#444444"/>
+              <path d="M49.1705 40.3723C50.1708 40.3723 50.9817 39.5614 50.9817 38.5612C50.9817 37.5609 50.1708 36.75 49.1705 36.75C48.1703 36.75 47.3594 37.5609 47.3594 38.5612C47.3594 39.5614 48.1703 40.3723 49.1705 40.3723Z" fill="#444444"/>
+              <path d="M38.2955 25.8789H32.8594V29.5039H38.2955V25.8789Z" fill="#444444"/>
+              <path d="M52.7877 25.8789H47.3516V29.5039H52.7877V25.8789Z" fill="#444444"/>
+              <path d="M16.5469 47.6224H23.7942V43.9974H16.5469V22.25H23.7942V18.625H16.5469C14.5458 18.625 12.9219 20.2489 12.9219 22.25V43.9974C12.9219 45.9985 14.5458 47.6224 16.5469 47.6224Z" fill="#444444"/>
+              <path d="M38.2942 18.6289H31.0469C29.0458 18.6289 27.4219 20.2528 27.4219 22.2539V44.0013C27.4219 46.0024 29.0458 47.6263 31.0469 47.6263H38.2942V44.0013H31.0469V22.2539H38.2942V18.6289Z" fill="#444444"/>
+              <path d="M45.5391 18.6289C43.538 18.6289 41.9141 20.2528 41.9141 22.2539V44.0013C41.9141 46.0024 43.538 47.6263 45.5391 47.6263H52.7891V44.0013H45.5391V22.2539H52.7891V18.6289H45.5391Z" fill="#444444"/>
+            </svg>
           </div>
           <h3 class="heading-2 col-start-2 col-span-1">
-          2 Million
+            Over 25% of NRAs
           </h3>
-          <p class="body-2 col-start-2 col-span-1 row-start-2">
-            If you stacked all the paperwork from one new drug application (2 million pages on average), the pile would be 200 meters tall.
-          </p>
+          <div class="col-start-2 col-span-1 row-start-2">
+            <p class="body-2 lg:max-w-[368px]">
+              Over 25% of NRAs across the globe have used the Accumulus platform.
+            </p>
+          </div>
         </div>
       </div>
-      <div class="col-span-12 md:col-span-5">
+      <div class="col-span-12 md:col-span-10 md:col-start-2 lg:col-span-5 lg:col-start-1">
         <div class="grid grid-cols-[64px, 1fr] gap-y-s2 lg:gap-y-s6 gap-x-s2 items-start">
           <div class="flex flex-col items-center justify-center col-start-1 col-span-1">                
-<svg width="65" height="65" viewBox="0 0 65 65" fill="none" xmlns="http://www.w3.org/2000/svg">
-<rect x="0.734375" y="0.734375" width="64" height="64" rx="10.9714" fill="#F5F5F5"/>
-<path d="M25.1955 32.7344H22.1953V41.7328H25.1955V32.7344Z" fill="#444444"/>
-<path d="M43.2033 32.7344H40.2031V41.7328H43.2033V32.7344Z" fill="#444444"/>
-<path d="M31.1955 32.7344C29.5393 32.7344 28.1953 34.0784 28.1953 35.7346V38.7348C28.1953 40.391 29.5393 41.735 31.1955 41.735H34.1957C35.8519 41.735 37.1959 40.391 37.1959 38.7348V35.7346C37.1959 34.0784 35.8519 32.7344 34.1957 32.7344H31.1955ZM34.1957 38.7326H31.1955V35.7324H34.1957V38.7326Z" fill="#444444"/>
-<path d="M46.1928 22.2344H19.1953V25.2346H46.1928V22.2344Z" fill="#444444"/>
-<path d="M41.6922 17.7344H23.6953V20.7346H41.6922V17.7344Z" fill="#444444"/>
-<path d="M46.193 26.7344H19.1955C17.5393 26.7344 16.1953 28.0784 16.1953 29.7346V44.7334C16.1953 46.3896 17.5393 47.7337 19.1955 47.7337H49.1932V44.7334H19.1955V29.7346H46.193V41.7332H49.1932V29.7346C49.1932 28.0784 47.8492 26.7344 46.193 26.7344Z" fill="#444444"/>
-</svg>
+            <svg width="65" height="65" viewBox="0 0 65 65" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <rect x="0.890625" y="0.625" width="64" height="64" rx="10.9714" fill="#F5F5F5"/>
+              <path d="M33.3906 48.6217C41.9513 48.6217 48.8906 41.6824 48.8906 33.1217C48.8906 32.3699 48.8351 31.6312 48.7305 30.9088H44.089C44.236 31.6247 44.3145 32.3634 44.3145 33.1217C44.3145 39.1459 39.4148 44.0456 33.3906 44.0456C27.3665 44.0456 22.4668 39.1459 22.4668 33.1217C22.4668 27.8559 26.2094 23.4498 31.1745 22.4234V35.3346H40.0293V30.9056H35.6002V17.625H33.3874C32.6356 17.625 31.8968 17.6806 31.1745 17.7852C23.6631 18.8606 17.8906 25.3162 17.8906 33.125C17.8906 41.6856 24.83 48.625 33.3906 48.625V48.6217Z" fill="#444444"/>
+            </svg>
           </div>
           <h3 class="heading-2 col-start-2 col-span-1">
-          2 Million
+            2.5 YEARS to 6.5 MONTHS
           </h3>
-          <p class="body-2 col-start-2 col-span-1 row-start-2">
-It costs ~$2B to bring a pharmaceutical product to market.
-          </p>
-        </div>
-      </div>
-      <div class="col-span-12 md:col-span-5 md:col-start-7">
-        <div class="grid grid-cols-[64px, 1fr] gap-y-s2 lg:gap-y-s6 gap-x-s2 items-start">
-          <div class="flex flex-col items-center justify-center col-start-1 col-span-1">                
-<svg width="65" height="65" viewBox="0 0 65 65" fill="none" xmlns="http://www.w3.org/2000/svg">
-<rect x="0.734375" y="0.734375" width="64" height="64" rx="10.9714" fill="#F5F5F5"/>
-<path d="M43.9881 21.8478C39.8369 17.6966 33.0797 17.6966 28.9284 21.8478L26.3352 24.441L23.8082 26.968L21.215 29.5612C17.0638 33.7125 17.0638 40.4697 21.215 44.621C25.3662 48.7722 32.1235 48.7722 36.2747 44.621L38.8679 42.0277L41.3949 39.5008L43.9881 36.9075C48.1394 32.7563 48.1394 25.999 43.9881 21.8478ZM38.8297 36.9356L36.3027 39.4625L33.7095 42.0557C30.9709 44.7943 26.5162 44.7943 23.7802 42.0557C21.0441 39.3172 21.0416 34.8625 23.7802 32.1264L26.3734 29.5332L28.8137 31.9734L31.3406 34.5004L33.8676 37.0274L36.3945 34.5004L33.8676 31.9734L31.3406 29.4465L28.9004 27.0062L31.4936 24.413C34.2322 21.6744 38.6869 21.6744 41.4229 24.413C44.1615 27.1516 44.1615 31.6063 41.4229 34.3423L38.8297 36.9356Z" fill="#444444"/>
-</svg>
+          <div class="col-start-2 col-span-1 row-start-2">
+            <p class="body-2 lg:max-w-[368px]">
+            ​ The first CMC PAC Reliance pilot supported by the Accumulus platform reduced the global approval and implementation timelines for a major drug substance process change from 2.5 YEARS to 6.5 MONTHS.
+            </p>
           </div>
-          <h3 class="heading-2 col-start-2 col-span-1">
-          140 Health Authority
-          </h3>
-          <p class="body-2 col-start-2 col-span-1 row-start-2">
-There are over 140 health authority jurisdictions that review and approve each product application.
-          </p>
         </div>
       </div>
     </div>
@@ -500,6 +491,8 @@ There are over 140 health authority jurisdictions that review and approve each p
     <a href="#" class="btn btn-tertiary">Read More</a>
   </div>
 </section>
+
+<!-- End statics -->
 
 <?php
   get_template_part(
--- template-parts/content-value-props.php
+++ template-parts/content-value-props.php
@@ -16,24 +16,24 @@
 				if($i % 2 == 1): 	
 			?>
 
-				<div class="grid grid-cols-12 gap-x-s2 gap-y-s6 lg:gap-y-0 pb-s6 lg:pb-s12 last-of-type:lg:!pb-0">
+				<div class="grid grid-cols-12 gap-x-s2 gap-y-s6 md:gap lg:gap-y-0 pb-s6 md:pb-s10 lg:pb-s12 last-of-type:max-md:pb-s8 last-of-type:lg:!pb-0">
 					<div class="row-start-2 md:row-start-1 col-span-12 md:col-span-5 lg:col-span-4 flex flex-col items-start gap-s3">
 
 						<?php  foreach($row['card_item'] as $card) : ?>
 
 							<?php if($card['acf_fc_layout'] == 'title') : ?>
-								<h3 class="text-h3Mobile md:text-h3Tablet lg:text-h3"><?php echo $card['title']; ?></h3>
+								<h3 class="heading-3"><?php echo $card['title']; ?></h3>
 							<?php endif; ?>
 
 							<?php if($card['acf_fc_layout'] == 'description'): ?>
-								<p class="text-b2"><?php echo $card['description']; ?></p>
+								<p class="body-2"><?php echo $card['description']; ?></p>
 							<?php endif; ?>
 							
 							<?php if($card['acf_fc_layout'] == 'bullet_list'): ?>
-								<ul class="flex flex-col gap-2 body-2">
+								<ul class="flex flex-col gap-2 text-b3Mobile md:text-b3Tablet lg:text-b3">
 
 									<?php foreach($card['bullet_list'] as $item): ?>
-									<li class="relative pl-3">
+									<li class="relative pl-3 body-3">
 										<span class="absolute left-0 top-2 block w-1 h-1 aspect-square bg-neutral-dgray rounded-full"></span>
 										<?php echo $item['item']; ?>
 									</li>
@@ -65,7 +65,7 @@
 
 				<?php else: ?>
 
-				<div class="grid grid-cols-12 gap-x-s2 gap-y-s6 lg:gap-y-0 pb-s6 lg:pb-s12 last-of-type:lg:!pb-0">
+				<div class="grid grid-cols-12 gap-x-s2 gap-y-s6 lg:gap-y-0 pb-s6 md:pb-s10 lg:pb-s12 last-of-type:max-md:pb-s8 last-of-type:lg:!pb-0">
 					<div class="col-span-12 md:col-span-6">
 
 						<?php foreach($row['card_item'] as $card) : ?>
@@ -82,18 +82,18 @@
 						<?php foreach($row['card_item'] as $card) : ?>
 
 							<?php if($card['acf_fc_layout'] == 'title') : ?>
-								<h3 class="text-h3Mobile md:text-h3Tablet lg:text-h3"><?php echo $card['title']; ?></h3>
+								<h3 class="heading-3"><?php echo $card['title']; ?></h3>
 							<?php endif; ?>
 
 							<?php if($card['acf_fc_layout'] == 'description'): ?>
-								<p class="text-b2"><?php echo $card['description']; ?></p>
+								<p class="body-2"><?php echo $card['description']; ?></p>
 							<?php endif; ?>
 							
 							<?php if($card['acf_fc_layout'] == 'bullet_list'): ?>
-								<ul class="flex flex-col gap-2 body-2">
+								<ul class="flex flex-col gap-2 text-b3Mobile md:text-b3Tablet lg:text-b3">
 
 									<?php foreach($card['bullet_list'] as $item): ?>
-									<li class="relative pl-3">
+									<li class="relative pl-3 body-3">
 										<span class="absolute left-0 top-2 block w-1 h-1 aspect-square bg-neutral-dgray rounded-full"></span>
 										<?php echo $item['item']; ?>
 									</li>
