2175694e6765d6ecee88198b40665a42f0af8916
Luis Barrera <autonomodeguate@gmail.com>
2024-08-20 22:51:47 -0600

    fix: bugs from about us page

--- template-parts/content-quote.php
+++ template-parts/content-quote.php
@@ -12,13 +12,35 @@
         <?php echo $args['sub_quote'] ?>
       </h3>
       <div class="flex items-start justify-normal <?php echo array_key_exists('stacked-author', $args) ? 'flex-col col-start-2 md:col-start-7 lg:col-start-7 col-end-11 md:col-end-9 lg:col-end-10' : 'flex-row col-start-1 md:col-start-6 lg:col-start-6 col-end-12 md:col-end-12 lg:col-end-12' ?> <?php echo array_key_exists('inverted', $args) ? 'row-start-1' : 'row-start-3' ?> gap-s2 <?php array_key_exists('references', $args) && 'pb-s6'; ?>">
-        <div class="relative w-40 md:w-[224px] aspect-square rounded-3xl bg-primary-glaciar overflow-hidden flex-shrink-0">
+
+        <figure data-post="1" class="select-none member [&>div>.close]:!flex flex flex-col items-start justify-stretch gap-s2 md:w-[224px]">
+          <img class="aspect-square w-full rounded-[40px] bg-[#EBEBEB] mb-s1" src="<?php echo $args['image']; ?>" alt="<?php echo $args['author']; ?>">
+          <div class="flex flex-row items-center justify-between w-full">
+            <h3 class="heading-6"><?php echo $args['author']; ?></h3>
+            <svg style="display:none;" class="open" width="17" height="18" viewBox="0 0 17 18" fill="none">
+              <path d="M7.83008 3.32325V4.95453V6.58461V8.21589V9.84717V11.4785V13.1085V14.7398V16.3711H9.46136V14.7398V13.1085V11.4785V9.84717V8.21589V6.58461V4.95453V3.32325V1.69197H7.83008V3.32325Z" fill="#444444"/>
+              <path d="M14.3467 8.21484H12.7154H11.0853H9.45403H7.82275H6.19147H4.56139H2.93011H1.29883V9.84612H2.93011H4.56139H6.19147H7.82275H9.45403H11.0853H12.7154H14.3467H15.978V8.21484H14.3467Z" fill="#444444"/>
+            </svg>
+            <svg style="display:none;" class="close" width="17" height="18" viewBox="0 0 17 18" fill="none">
+              <path d="M14.3467 8.21094H12.7154H11.0853H9.45403H7.82275H6.19147H4.56139H2.93011H1.29883V9.84222H2.93011H4.56139H6.19147H7.82275H9.45403H11.0853H12.7154H14.3467H15.978V8.21094H14.3467Z" fill="#444444"/>
+            </svg>     	
+          </div>
+          <span class="body-4 text-neutral-sgray"><?php echo $args['position']; ?></span>
+          <div style="display:none;" class="summary flex-col items-start gap-s2 col-span-2">
+            <p class="body-4 text-neutral-sgray">
+              <?php echo $args['description']; ?>
+            </p>
+            <a href="<?php echo $args['linkedin']; ?>" target="_blank">
+              <svg width="16" height="16" viewBox="0 0 16 16" fill="none">
+                <path d="M0.738907 5.15332H3.92293V15.3947H0.738907V5.15332ZM2.331 0.0625C3.34866 0.0625 4.17568 0.889524 4.17568 1.90888C4.17568 2.92722 3.34866 3.75424 2.331 3.75424C1.31096 3.75424 0.486328 2.92722 0.486328 1.90888C0.486328 0.889524 1.31096 0.0625 2.331 0.0625Z" fill="#444444"/>
+                <path d="M5.91797 5.15399H8.9711V6.55444H9.01461C9.43956 5.74892 10.4782 4.90039 12.0263 4.90039C15.2495 4.90039 15.8445 7.02086 15.8445 9.7784V15.3954H12.6628V10.415C12.6628 9.22699 12.6422 7.6994 11.0088 7.6994C9.35235 7.6994 9.0996 8.99421 9.0996 10.3296V15.3954H5.91797V5.15399Z" fill="#444444"/>
+              </svg>
+            </a>
+          </div>
+        </figure>
+
+        <!-- <div class="relative w-40 md:w-[224px] aspect-square rounded-3xl bg-primary-glaciar overflow-hidden flex-shrink-0">
           <img class="w-full h-full" src="<?php echo $args['image']; ?>" alt="<?php echo $args['author']; ?>" />
-          <svg class="absolute bottom-s3 right-s3" width="53" height="53" viewBox="0 0 53 53" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <rect x="0.03125" y="0.619141" width="52" height="52" rx="14" fill="#12D0FF"/>
-            <path d="M37.6914 22.9769C37.543 22.9769 37.2686 22.99 37.1238 23.0107C33.6938 23.4167 29.839 21.0767 29.2884 16.5886C29.0196 14.352 27.1101 12.6191 24.8002 12.6191C22.3024 12.6191 20.2782 14.6433 20.2782 17.1411C20.2782 17.6298 20.3571 18.1034 20.5019 18.547C21.6709 22.1236 21.2198 24.9973 15.7769 26.7771C13.0216 27.6774 11.0312 30.2617 11.0312 33.3177C11.0312 37.118 14.1136 40.2003 17.9138 40.2003C21.7141 40.2003 24.7964 37.118 24.7964 33.3177C24.7964 32.2389 24.5427 31.2277 24.0992 30.3181C23.7721 29.6377 23.5316 29.0156 23.3568 28.4386C23.291 28.2694 23.2365 28.0909 23.1914 27.9123C23.1839 27.8785 23.1745 27.8409 23.1669 27.809C23.1463 27.7225 23.1294 27.6398 23.1124 27.5515C23.088 27.4274 23.0711 27.3109 23.0579 27.2075C23.0579 27.1793 23.0504 27.1493 23.0504 27.1211C23.0128 26.764 23.0297 26.5328 23.0297 26.5328C23.0222 25.5329 23.3417 24.6045 23.8887 23.8245C24.7964 22.4807 26.332 21.5936 28.078 21.5936C28.1362 21.5936 28.1945 21.6011 28.2528 21.6011C28.5272 21.6011 28.8053 21.6349 29.0816 21.6932C29.1267 21.7007 29.1681 21.7139 29.2151 21.727C29.3147 21.7515 29.4143 21.7721 29.5139 21.806C29.6379 21.8435 29.762 21.8887 29.8823 21.9338C29.9067 21.9413 29.9368 21.9507 29.9612 21.962C30.0533 21.9995 30.1398 22.0409 30.23 22.0822C30.7487 22.3191 31.243 22.6292 31.6809 22.9994C31.726 23.037 31.7674 23.0746 31.8144 23.1065C31.8595 23.1441 31.9102 23.1855 31.9516 23.2231C33.0999 24.2023 33.8592 25.5705 34.1505 27.0929C34.4531 28.698 35.8627 29.914 37.5599 29.914C39.4769 29.914 41.0312 28.3578 41.0312 26.4426C41.0312 24.5274 39.616 23.0201 37.6914 22.9825" fill="#FCFCFC"/>
-            <path d="M31.5371 26.6377C31.5371 28.5491 29.9865 30.1015 28.0732 30.1015C26.1599 30.1015 24.6094 28.551 24.6094 26.6377C24.6094 24.7244 26.1599 23.1738 28.0732 23.1738C29.9865 23.1738 31.5371 24.7244 31.5371 26.6377Z" fill="#FCFCFC"/>
-          </svg>
         </div>
         <div class="flex flex-col items-start gap-s2">
           <p class="<?php echo array_key_exists('stacked-author', $args) ? 'heading-6' : 'heading-3' ?>">
@@ -27,7 +49,7 @@
           <p class="<?php echo array_key_exists('stacked-author', $args) ? 'body-4' : 'body-3' ?>">
             <?php echo $args['position']; ?>
           </p>
-        </div>
+        </div> -->
       </div>
       <?php if (array_key_exists('references', $args)) { ?>
       <ul class="flex flex-col items-start col-start-1 md:col-start-2 col-span-12 md:col-span-10 <?php echo array_key_exists('inverted', $args) ? 'row-start-4' : '' ?>">
@@ -51,3 +73,19 @@
   </div>
 </section>
 <!-- END QUOTE -->
+
+<script>
+	document.addEventListener('DOMContentLoaded', function() {
+		let teamMembers = document.querySelectorAll("figure.member")	
+		teamMembers.forEach(function(teamMember) {
+			let post = teamMember.dataset.post
+			teamMember.addEventListener('click', function(){
+				let toggleClass = teamMember.classList.contains("[&>div>.close]:!flex") ? 'open' : 'close';
+				teamMember.classList.remove('[&>div>.close]:!flex')
+				teamMember.classList.remove('[&>div>.open]:!flex')
+				teamMember.classList.toggle('[&>.summary]:!flex')
+				teamMember.classList.toggle('[&>div>.'+toggleClass+']:!flex')
+			})	
+		})
+	})
+</script>	
