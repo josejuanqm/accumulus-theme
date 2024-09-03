aa93e39980624e457910cc99c3b6c458951569e1
Luis Barrera <autonomodeguate@gmail.com>
2024-09-02 22:00:05 -0600

    feat: adding redirect to download free pdf after sending

--- functions.php
+++ functions.php
@@ -6518,3 +6518,26 @@ add_action( 'acf/init', function() {
 	) );
 } );
 
+
+/* 
+
+* Gravity forms
+
+*/
+
+add_filter( 'gform_confirmation', function ( $confirmation, $form, $entry, $ajax ) {
+	GFCommon::log_debug( 'gform_confirmation: running.' );
+
+	$forms = array( 1, 2 ); // Target forms with id 3, 6 and 7. Change this to fit your needs.
+
+	if ( ! in_array( $form['id'], $forms ) || empty( $confirmation['redirect'] ) ) {
+			return $confirmation;
+	}
+
+	$url = esc_url_raw( $confirmation['redirect'] );
+	GFCommon::log_debug( __METHOD__ . '(): Redirect to URL: ' . $url );
+	$confirmation = 'Thanks for contacting us! We will get in touch with you shortly.';
+	$confirmation .= GFCommon::get_inline_script_tag( "window.open('$url', '_blank');" );
+
+	return $confirmation;
+}, 10, 4 );
