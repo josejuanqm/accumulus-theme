e91f3c22b5d36d6dd3fe7ca1d2cd028868789c10
Luis Barrera <autonomodeguate@gmail.com>
2024-09-04 13:13:39 -0600

    feat: Platform - adding case study statistics section and removing testimonial section

--- template-parts/content-platform.php
+++ template-parts/content-platform.php
@@ -60,6 +60,98 @@
 <!-- Main banner -->
 
 
+<!-- Statistics -->
+
+
+<section class="section relative w-full py-s10 bg-secondary-aqua flex flex-col gap-s9">
+  <div class="container mx-auto">
+    <div class="grid grid-cols-12 gap-x-s2 gap-y-s6 lg:py-s6 md:py-s8">
+      <h4 class="col-span-12 heading-4 uppercase">CASE STUDY STATISTICS</h4>
+      <h2 class="grid grid-cols-12 grid-rows-2 col-span-12 heading-1">
+        <span class="block col-span-12">
+          Let's Talk
+        </span>
+        <span class="block col-span-12">
+          Numbers
+        </span>
+      </h2>
+    </div>
+    <div class="grid grid-cols-12 gap-x-s2 gap-y-s6 md:gap-y-s8 lg:py-s6 pt-s4 md:pt-0">
+      <div class="col-span-12 md:col-span-10 md:col-start-2 lg:col-span-5 lg:col-start-1">
+        <div class="grid grid-cols-[64px, 1fr] gap-y-s2 lg:gap-y-s6 gap-x-s2 items-start">
+          <div class="flex flex-col items-center justify-center col-start-1 col-span-1">                
+						<svg width="65" height="65" viewBox="0 0 65 65" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<rect x="0.03125" y="0.0585938" width="64" height="64" rx="10.9714" fill="#F5F5F5"/>
+							<path d="M43.285 21.172C39.1338 17.0208 32.3765 17.0208 28.2253 21.172L25.6321 23.7653L23.1051 26.2922L20.5119 28.8855C16.3606 33.0367 16.3606 39.7939 20.5119 43.9452C24.6631 48.0964 31.4203 48.0964 35.5716 43.9452L38.1648 41.3519L40.6918 38.825L43.285 36.2317C47.4362 32.0805 47.4362 25.3233 43.285 21.172ZM38.1266 36.2598L35.5996 38.7867L33.0064 41.38C30.2678 44.1186 25.8131 44.1186 23.0771 41.38C20.341 38.6414 20.3385 34.1867 23.0771 31.4507L25.6703 28.8574L28.1106 31.2977L30.6375 33.8246L33.1645 36.3516L35.6914 33.8246L33.1645 31.2977L30.6375 28.7707L28.1973 26.3305L30.7905 23.7372C33.5291 20.9986 37.9838 20.9986 40.7198 23.7372C43.4584 26.4758 43.4584 30.9305 40.7198 33.6665L38.1266 36.2598Z" fill="#444444"/>
+						</svg>
+          </div>
+          <h3 class="heading-2 col-start-2 col-span-1">
+						1 Submission
+          </h3>
+          <div class="col-start-2 col-span-1 row-start-2">
+            <p class="body-2 lg:max-w-[368px]">
+							PAC Submission Project prepared by a leading biopharmaceutical company
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-span-12 md:col-span-10 lg:col-span-7 md:col-start-2 lg:col-start-7">
+        <div class="grid grid-cols-[64px, 1fr] gap-y-s2 lg:gap-y-s6 gap-x-s2 items-start">
+          <div class="flex flex-col items-center justify-center col-start-1 col-span-1">                
+						<svg width="65" height="65" viewBox="0 0 65 65" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<rect x="0.03125" y="0.0585938" width="64" height="64" rx="10.9714" fill="#F5F5F5"/>
+							<path d="M44.9178 28.2649H18.9102V22.9345L31.914 18.0586L44.9178 22.9345V28.2649ZM21.7909 25.3842H42.0371V24.9318L31.914 21.1356L21.7909 24.9318V25.3842Z" fill="#444444"/>
+							<path d="M24.6815 31.1602H21.8008V41.279H24.6815V31.1602Z" fill="#444444"/>
+							<path d="M42.0292 31.1602H39.1484V41.279H42.0292V31.1602Z" fill="#444444"/>
+							<path d="M36.244 31.1602H33.3633V41.279H36.244V31.1602Z" fill="#444444"/>
+							<path d="M30.4667 31.1602H27.5859V41.279H30.4667V31.1602Z" fill="#444444"/>
+							<path d="M44.9245 44.1748H18.9062V47.0555H44.9245V44.1748Z" fill="#444444"/>
+						</svg>
+          </div>
+          <h3 class="heading-2 col-start-2 col-span-1">
+						48 NRAs
+          </h3>
+          <div class="col-start-2 col-span-1 row-start-2">
+            <p class="body-2 lg:max-w-[368px]">
+							National Regulatory Authorities agree to participate in a Reliance project for the PAC submission
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-span-12 md:col-span-10 md:col-start-2 lg:col-span-5 lg:col-start-1">
+        <div class="grid grid-cols-[64px, 1fr] gap-y-s2 lg:gap-y-s6 gap-x-s2 items-start">
+          <div class="flex flex-col items-center justify-center col-start-1 col-span-1">                
+						<svg width="65" height="65" viewBox="0 0 65 65" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<rect x="0.03125" y="0.853516" width="64" height="64" rx="10.9714" fill="#F5F5F5"/>
+							<path d="M26.2308 29.4766H22.3555V33.3519H26.2308V29.4766Z" fill="#444444"/>
+							<path d="M26.2308 37.2275H22.3555V41.1029H26.2308V37.2275Z" fill="#444444"/>
+							<path d="M33.9847 37.2275H30.1094V41.1029H33.9847V37.2275Z" fill="#444444"/>
+							<path d="M41.7347 37.2275H37.8594V41.1029H41.7347V37.2275Z" fill="#444444"/>
+							<path d="M33.9847 29.4766H30.1094V33.3519H33.9847V29.4766Z" fill="#444444"/>
+							<path d="M41.7347 29.4766H37.8594V33.3519H41.7347V29.4766Z" fill="#444444"/>
+							<path d="M45.6066 21.7289H41.7313V17.8535H37.8559V21.7289H26.2326V17.8535H22.3573V21.7289H18.4819C16.3454 21.7289 14.6094 23.4621 14.6094 25.6014V44.9754H18.4847V25.6043H45.6066V44.9783H22.3573V48.8537H45.6066C47.746 48.8537 49.482 47.1176 49.482 44.9783V25.6043C49.482 23.4649 47.746 21.7289 45.6066 21.7289Z" fill="#444444"/>
+						</svg>
+          </div>
+          <h3 class="heading-2 col-start-2 col-span-1">
+						100 Weeks
+          </h3>
+          <div class="col-start-2 col-span-1 row-start-2">
+            <p class="body-2 lg:max-w-[368px]">100 weeks saved from the average time</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="flex flex-col items-center text-center justify-stretch md:justify-normal gap-s4">
+    <a href="#" class="btn btn-tertiary">Read More</a>
+  </div>
+</section>
+
+
+<!-- End statics -->
+
+
 <?php 
 
 $value_propositions = get_field('value_propositions');
@@ -201,12 +293,13 @@ $value_propositions = get_field('value_propositions');
 
 <!-- Values -->
 
-
 <?php 
-	$key_features = get_field('key_features');
-?>
 
-<?php if($key_features): ?>
+$key_features = get_field('key_features');
+
+if($key_features): 
+
+?>
 <section class="relative section w-full pt-s8 md:pt-s12 pb-s8 md:pb-s6 lg:pb-s12 bg-neutral-nude">
 	<div class="container mx-auto">
 		<div class="grid grid-cols-12 gap-x-s2 gap-y-s6 pb-s7">
@@ -298,34 +391,6 @@ $value_propositions = get_field('value_propositions');
 <?php endif; ?>
 
 
-<?php 
-
-  // Fields from testimonial
-
-  $testimonial_platform = get_field('testimonial_platform');
-
-?>
-
-<?php if($testimonial_platform) : ?>
-<section class="relative section w-full pt-s8 md:pt-s10 lg:pt-s12 pb-s8 md:pb-s10 lg:pb-s12 bg-neutral-nwhite">
-  <div class="container mx-auto">
-    <div class="grid grid-cols-6 md:grid-cols-12 gap-x-s2 gap-y-s6">
-      <div class="col-span-6 md:col-span-7 lg:col-span-6 md:col-start-6 lg:col-start-6 lg:pl-s6 flex items-center justify-between">
-        <img class="max-w-[145px] md:max-w-full rounded-large" src="<?php echo $testimonial_platform['image']; ?>" alt="<?php echo $testimonial_platform['name']; ?>" />
-        <div class="flex flex-col pl-s2 gap-s2">
-          <h4 class="heading-3 text-neutral-dgray w-full md:max-w-[195px]"><?php echo $testimonial_platform['name']; ?></h4>
-          <p class="body-3"><?php echo $testimonial_platform['position']; ?></p>
-        </div>
-      </div>
-      <h2 class="col-span-6 md:col-span-10 lg:col-span-9 heading-2 text-neutral-dgray">
-      <?php echo $testimonial_platform['testimonial']; ?>
-      </h2>
-      <p class="col-span-5 md:col-span-8 md:col-start-3 text-b1Mobile md:text-b1Tablet lg:text-b1"><?php echo $testimonial_platform['resume_text']; ?></p>
-    </div>
-  </div>
-</section>
-<?php endif; ?>
-<!-- Testimonial -->
 
 <?php 
   $benefits = get_field('benefits');
