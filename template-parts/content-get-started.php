ab532d1e5ca43c0ef08a5bc9d2c07bc7161d9711
Luis Barrera <autonomodeguate@gmail.com>
2024-09-05 23:18:23 -0600

    feat: get started page - updates

--- template-parts/content-get-started.php
+++ template-parts/content-get-started.php
@@ -18,6 +18,7 @@
 	$title_tag = get_field('title_tag');
 	$main_title = get_field('main_title');
 	$resume_text = get_field('resume_text');
+	$text_cta = get_field('text_cta');
 	$link_learn_more = get_field('link_learn_more');
 ?>
 
@@ -51,157 +52,168 @@
 <!-- Main banner -->
 
 
-<section class="relative section w-full pt-s12 md:pt-s10 lg:pb-s12">
+<section class="bg-neutral-nude">
 
-	<div class="container mx-auto">
+<?php 
 
-    <div class="w-full pb-s6">
-      <h4 class="text-h4Mobile md:text-h4Tablet lg:text-h4">HOW YOU CAN GET INVOLVED</h4>
+$get_started_content = get_field('get_started_content');
+
+foreach($get_started_content as $row) :
+
+?>
+
+<!-- Join us -->
+
+
+<?php 
+
+if($row['acf_fc_layout'] == 'join_the_forum_layout') :
+
+$join_the_forum = $row['join_the_forum'];
+
+if($join_the_forum):
+
+?>
+
+<section class="relative section w-full pt-s12 md:pt-s10 pb-s10 md:pb-s12 bg-neutral-nwhite">
+	<div class="container mx-auto px-s2 md:px-0">
+		<div class="grid grid-cols-6 md:grid-cols-12 lg:grid-cols-12 gap-s2 md:gap-x-s2 md:gap-s10 lg:gap-s12">
+
+      <h2 class="heading-1 font-normal col-span-6 md:col-span-12 grid grid-cols-6 md:grid-cols-12 gap-s2 lg:pb-s1">
+        <span class="col-span-6 md:col-span-12">
+          <?php echo $join_the_forum['first_line_title']; ?>
+        </span>
+        <span class="col-span-6 md:col-span-10 lg:col-span-10 col-start-2 md:col-start-2 lg:col-start-2">
+          <?php echo $join_the_forum['second_line_title']; ?>
+        </span>
+      </h2>
+
+      <?php foreach($join_the_forum['steps_join'] as $item) : ?>
+      <div class="col-span-6 md:col-span-7 lg:col-span-7 col-start-1 md:col-start-5 lg:col-start-6 flex items-start gap-s3">
+        <img class="w-[55px] md:w-[64px] aspect-square" src="<?php echo $item['icon']; ?>" alt="<?php echo $item['title']; ?>" />
+        <div class="description-wysiwyg w-auto">
+          <h3 class="heading-2 pb-s2"><?php echo $item['title']; ?></h3>
+          <?php echo $item['description']; ?>
+        </div>
+      </div>
+      <?php endforeach; ?>
+
+			<div class="col-span-6 md:col-span-7 lg:col-span-2 col-start-1 md:col-start-5 lg:col-start-6">
+				<a class="btn-secondary" href="<?php echo $join_the_forum['link_cta']; ?>"><?php echo $join_the_forum['text_cta']; ?></a>
+			</div>
+
     </div>
-		
-		<?php
-			// Values view
-
-			$values_row = get_field('values_row');
-
-			$i = 0;		
-			
-			foreach($values_row as $row) :
-				$i++;
-
-				if($i % 2 == 1): 	
-			?>
-
-				<div class="grid grid-cols-12 gap-x-s2 gap-y-s6 lg:gap-y-0 pb-s6 md:pb-s10 lg:pb-s12 last-of-type:max-md:pb-s8 last-of-type:lg:!pb-0">
-					<div class="row-start-2 md:row-start-1 col-span-12 md:col-span-5 lg:col-span-4 flex flex-col items-start gap-s3">
-
-						<?php  foreach($row['card_item'] as $card) : ?>
-
-							<?php if($card['acf_fc_layout'] == 'title') : ?>
-								<h3 class="text-h3Mobile md:text-h3Tablet lg:text-h3"><?php echo $card['title']; ?></h3>
-							<?php endif; ?>
-
-							<?php if($card['acf_fc_layout'] == 'description'): ?>
-								<p class="body-2"><?php echo $card['description']; ?></p>
-							<?php endif; ?>
-							
-							<?php if($card['acf_fc_layout'] == 'bullet_list'): ?>
-								<ul class="flex flex-col gap-2 body-2">
-
-									<?php foreach($card['bullet_list'] as $item): ?>
-									<li class="relative pl-3">
-										<span class="absolute left-0 top-2 lg:top-[10px] block w-1 h-1 aspect-square bg-neutral-dgray rounded-full"></span>
-										<?php echo $item['item']; ?>
-									</li>
-									<?php endforeach; ?>
-
-								</ul>
-							<?php endif; ?>
-
-							<?php if($card['acf_fc_layout'] == 'cta'): ?>
-								<a class="btn-tertiary" href="<?php echo $card['link']; ?>"><?php echo $card['text_link']; ?></a>
-							<?php endif; ?>
-
-						<?php endforeach; ?>
-
-					</div>
-					<div class="row-start-1 col-span-12 md:col-span-6 md:col-start-7">
-
-						<?php foreach($row['card_item'] as $card) : ?>
-
-							<?php if ($card['acf_fc_layout'] == 'image') : ?>
-								<img src="<?php echo $card['image']; ?>"  />
-							<?php endif; ?>
-
-						<?php endforeach; ?>
-
-					</div>
-				</div>
-				<!-- text left - img right -->
-
-				<?php else: ?>
-
-				<div class="grid grid-cols-12 gap-x-s2 gap-y-s6 lg:gap-y-0 pb-s6 md:pb-s10 lg:pb-s12 last-of-type:max-md:pb-s8 last-of-type:lg:!pb-0">
-					<div class="col-span-12 md:col-span-6">
-
-						<?php foreach($row['card_item'] as $card) : ?>
-
-							<?php if ($card['acf_fc_layout'] == 'image') : ?>
-								<img src="<?php echo $card['image']; ?>"  />
-							<?php endif; ?>
-
-						<?php endforeach; ?>
-
-					</div>
-					<div class="col-span-12 md:col-span-5 lg:col-span-4 md:col-start-8 lg:col-start-8 flex flex-col items-start gap-s3">
-
-						<?php foreach($row['card_item'] as $card) : ?>
-
-							<?php if($card['acf_fc_layout'] == 'title') : ?>
-								<h3 class="text-h3Mobile md:text-h3Tablet lg:text-h3"><?php echo $card['title']; ?></h3>
-							<?php endif; ?>
-
-							<?php if($card['acf_fc_layout'] == 'description'): ?>
-								<p class="body-2"><?php echo $card['description']; ?></p>
-							<?php endif; ?>
-							
-							<?php if($card['acf_fc_layout'] == 'bullet_list'): ?>
-								<ul class="flex flex-col gap-2 body-2">
-
-									<?php foreach($card['bullet_list'] as $item): ?>
-									<li class="relative pl-3">
-										<span class="absolute left-0 top-2 lg:top-[10px] block w-1 h-1 aspect-square bg-neutral-dgray rounded-full"></span>
-										<?php echo $item['item']; ?>
-									</li>
-									<?php endforeach; ?>
-
-								</ul>
-							<?php endif; ?>
-
-							<?php if($card['acf_fc_layout'] == 'cta'): ?>
-								<a class="btn-tertiary" href="<?php echo $card['link']; ?>"><?php echo $card['text_link']; ?></a>
-							<?php endif; ?>
-
-						<?php endforeach; ?>
-
-					</div>
-				</div>
-				<!-- img left - text right -->
-
-				<?php endif; ?>
-
-		<?php
-			endforeach;
-		?>
-
-	</div>
-
+  </div>
 </section>
 
-<!-- Values -->
+<?php endif; ?>
 
-<section class="relative section w-full pt-s12 pb-s12 bg-neutral-nude bg-get-started-section-mobile md:bg-get-started-section-tablet lg:bg-get-started-section-desktop bg-cover bg-no-repeat bg-center">
+<?php endif; ?>
+
+<!-- end Join us -->
+
+
+<!-- Discover -->
+
+<?php 
+
+if($row['acf_fc_layout'] == 'benefits_layout') :
+
+	$benefits = $row['benefits'];
+
+	if($benefits):
+?>
+
+<section class="section relative w-full bg-neutral-nude">
+	<div class="container mx-auto">
+
+		<div class="grid grid-cols-12 gap-x-s2 gap-y-s4 pt-s10 md:pt-s12 md:pb-s6">
+			<h2 class="grid grid-cols-12 grid-rows-2 col-span-12 gap-s2 heading-1 justify-start items-start">
+				<span class="col-span-12">
+					<?php echo $benefits['title_first_line']; ?>
+				</span>
+				<span class="col-span-10 col-start-2 md:col-start-3">
+					<?php echo $benefits['title_second_line']; ?>
+				</span>
+			</h2>
+		</div>
+		<!-- Title -->
+
+
+		<div class="grid grid-cols-12 gap-x-s2 gap-y-s6 md:gap-y-s10 pt-s4 lg:pt-s6 md:pt-0">
+			
+			<?php 
+				foreach($benefits['benefit_list'] as $item): 
+			?>
+			<div class="col-span-12 md:col-span-4">
+
+				<div class="grid grid-cols-6 md:grid-cols-4 lg:grid-cols-6 gap-y-s2 lg:gap-y-s6 gap-x-s2">
+
+					<div class="col-start-1 col-span-1 flex flex-col">
+						<img class="w-[39px] md:w-[64px] aspect-square" src="<?php echo $item['icon'] ?>" alt="<?php echo $item['title'] ?>" />
+					</div>
+
+					<div class="col-start-2 col-span-5 md:col-span-3 lg:col-span-5">
+						<h3 class="heading-7 md:max-w-[220px] lg:max-w-[272px]"><?php echo $item['title'] ?></h3>
+					</div>
+
+				</div>
+
+			</div>
+			<?php endforeach; ?>
+
+			<p class="col-span-12 md:col-span-7 lg:col-span-6 md:col-start-6 lg:col-start-6 body-2">
+				<?php echo $benefits['resume']; ?>
+		</div>
+		<!-- Items -->
+
+	</div>
+</section>
+
+<?php 
+	endif;
+endif; 
+?>
+
+<!-- End Discover -->
+
+
+<?php
+
+if($row['acf_fc_layout'] == 'ready_to_join_us_layout') :
+
+?>
+
+<!-- Get statted section -->
+
+<section class="relative section w-full pt-s12 pb-s12 bg-neutral-nude bg-get-started-section-mobile md:bg-get-started-section-tablet lg:bg-get-started-section-desktop bg-cover bg-no-repeat bg-center mt-s8 md:mt-s12">
 	<div class="container mx-auto">
 
 		<div class="grid grid-cols-12 gap-x-s2 gap-y-s3 md:gap-y-s6">
 
-			<h6 class="col-span-12 text-h4Mobile md:text-h4Tablet lg:text-h4 uppercase">GET STARTED</h6>
-			<h2 class="col-span-12 text-h2Mobile md:text-h2Tablet lg:text-h2">Ready to Join Us?</h2>
-			<p class="col-span-12 md:col-span-6 lg:col-span-4 text-b3Mobile md:text-b3Tablet lg:text-b3 py-s2 md:py-0">
-			Take the first step toward transforming the life sciences-regulatory ecosystem. 
-				<br /><br />
-				Together, let’s make a difference!
-			</p>
+			<h6 class="col-span-12 text-h4Mobile md:text-h4Tablet lg:text-h4 uppercase"><?php echo $row['eyebrown']; ?></h6>
+			<h2 class="col-span-12 text-h2Mobile md:text-h2Tablet lg:text-h2"><?php echo $row['title']; ?></h2>
+			<p class="col-span-12 md:col-span-6 lg:col-span-4 text-b3Mobile md:text-b3Tablet lg:text-b3 py-s2 md:py-0"><?php echo $row['resume']; ?></p>
 			<div class="col-span-12 flex justify-start">
-				<a href="#" class="btn-tertiary">Get Started</a>
+				<a href="<?php echo $row['link_cta']; ?>" class="btn-tertiary"><?php echo $row['text_cta']; ?></a>
 			</div>
 
 		</div>
 
 	</div>
 </section>
-<?php //include get_theme_file_path( 'components/get-started-section.php' ); ?>
-<!-- Get statted section -->
+
+<?php 
+endif; 
+?>
+
+<!-- End Get statted section -->
+
+<?php endforeach; ?>
+
+</section>
+
+
 
 
 <!-- Form Module -->
