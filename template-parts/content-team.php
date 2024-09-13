5db1c97ca95408d233f7fb7110b564d835aba3ef
Luis Barrera <autonomodeguate@gmail.com>
2024-09-13 14:22:43 -0600

    fix: updating team page

--- template-parts/content-team.php
+++ template-parts/content-team.php
@@ -47,19 +47,20 @@
           <img class="aspect-square w-full rounded-[40px] bg-[#EBEBEB] mb-s1" src="<?php echo $team_member['image']; ?>" alt="<?php echo $team_member['name']; ?>">
           <div class="flex flex-row items-center justify-between w-full">
             <h3 class="heading-6"><?php echo $team_member['name']; ?></h3>
-            <svg style="display:none;" class="open" width="17" height="18" viewBox="0 0 17 18" fill="none">
+            <!-- <svg style="display:none;" class="open" width="17" height="18" viewBox="0 0 17 18" fill="none">
               <path d="M7.83008 3.32325V4.95453V6.58461V8.21589V9.84717V11.4785V13.1085V14.7398V16.3711H9.46136V14.7398V13.1085V11.4785V9.84717V8.21589V6.58461V4.95453V3.32325V1.69197H7.83008V3.32325Z" fill="#444444"/>
               <path d="M14.3467 8.21484H12.7154H11.0853H9.45403H7.82275H6.19147H4.56139H2.93011H1.29883V9.84612H2.93011H4.56139H6.19147H7.82275H9.45403H11.0853H12.7154H14.3467H15.978V8.21484H14.3467Z" fill="#444444"/>
-            </svg>
-            <svg style="display:none;" class="close" width="17" height="18" viewBox="0 0 17 18" fill="none">
+            </svg> -->
+            <!-- <svg style="display:none;" class="close" width="17" height="18" viewBox="0 0 17 18" fill="none">
               <path d="M14.3467 8.21094H12.7154H11.0853H9.45403H7.82275H6.19147H4.56139H2.93011H1.29883V9.84222H2.93011H4.56139H6.19147H7.82275H9.45403H11.0853H12.7154H14.3467H15.978V8.21094H14.3467Z" fill="#444444"/>
-            </svg>     	
+            </svg> -->
           </div>
           <span class="body-4 text-neutral-sgray"><?php echo $team_member['position']; ?></span>
-          <div style="display:none;" class="summary flex-col items-start gap-s2 col-span-2">
-            <p class="body-4 text-neutral-sgray">
-              <?php echo $team_member['description']; ?>
-            </p>
+          <!-- <div style="display:none;" class="summary flex-col items-start gap-s2 col-span-2"> -->
+          <div style="display:block;" class="summarys flex-col items-start gap-s2 col-span-2">
+            <!-- <p class="body-4 text-neutral-sgray">
+              <?php // echo $team_member['description']; ?>
+            </p> -->
             <a href="<?php echo $team_member['linkedin']; ?>" target="_blank">
               <svg width="16" height="16" viewBox="0 0 16 16" fill="none">
                 <path d="M0.738907 5.15332H3.92293V15.3947H0.738907V5.15332ZM2.331 0.0625C3.34866 0.0625 4.17568 0.889524 4.17568 1.90888C4.17568 2.92722 3.34866 3.75424 2.331 3.75424C1.31096 3.75424 0.486328 2.92722 0.486328 1.90888C0.486328 0.889524 1.31096 0.0625 2.331 0.0625Z" fill="#444444"/>
@@ -98,19 +99,20 @@
           <img class="aspect-square w-full rounded-[40px] bg-[#EBEBEB] mb-s1" src="<?php echo $team_member['image']; ?>" alt="<?php echo $team_member['name']; ?>">
           <div class="flex flex-row items-center justify-between w-full">
             <h3 class="heading-6"><?php echo $team_member['name']; ?></h3>
-            <svg style="display:none;" class="open" width="17" height="18" viewBox="0 0 17 18" fill="none">
+            <!-- <svg style="display:none;" class="open" width="17" height="18" viewBox="0 0 17 18" fill="none">
               <path d="M7.83008 3.32325V4.95453V6.58461V8.21589V9.84717V11.4785V13.1085V14.7398V16.3711H9.46136V14.7398V13.1085V11.4785V9.84717V8.21589V6.58461V4.95453V3.32325V1.69197H7.83008V3.32325Z" fill="#444444"/>
               <path d="M14.3467 8.21484H12.7154H11.0853H9.45403H7.82275H6.19147H4.56139H2.93011H1.29883V9.84612H2.93011H4.56139H6.19147H7.82275H9.45403H11.0853H12.7154H14.3467H15.978V8.21484H14.3467Z" fill="#444444"/>
-            </svg>
-            <svg style="display:none;" class="close" width="17" height="18" viewBox="0 0 17 18" fill="none">
+            </svg> -->
+            <!-- <svg style="display:none;" class="close" width="17" height="18" viewBox="0 0 17 18" fill="none">
               <path d="M14.3467 8.21094H12.7154H11.0853H9.45403H7.82275H6.19147H4.56139H2.93011H1.29883V9.84222H2.93011H4.56139H6.19147H7.82275H9.45403H11.0853H12.7154H14.3467H15.978V8.21094H14.3467Z" fill="#444444"/>
-            </svg>     	
+            </svg> -->
           </div>
           <span class="body-4 text-neutral-sgray"><?php echo $team_member['position']; ?></span>
-          <div style="display:none;" class="summary flex-col items-start gap-s2 col-span-2">
-            <p class="body-4 text-neutral-sgray">
-              <?php echo $team_member['description']; ?>
-            </p>
+          <!-- <div style="display:none;" class="summary flex-col items-start gap-s2 col-span-2"> -->
+          <div style="display:block;" class="summarys flex-col items-start gap-s2 col-span-2">
+            <!-- <p class="body-4 text-neutral-sgray">
+              <?php //echo $team_member['description']; ?>
+            </p> -->
             <a href="<?php echo $team_member['linkedin']; ?>" target="_blank">
               <svg width="16" height="16" viewBox="0 0 16 16" fill="none">
                 <path d="M0.738907 5.15332H3.92293V15.3947H0.738907V5.15332ZM2.331 0.0625C3.34866 0.0625 4.17568 0.889524 4.17568 1.90888C4.17568 2.92722 3.34866 3.75424 2.331 3.75424C1.31096 3.75424 0.486328 2.92722 0.486328 1.90888C0.486328 0.889524 1.31096 0.0625 2.331 0.0625Z" fill="#444444"/>
